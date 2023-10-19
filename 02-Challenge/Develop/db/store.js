d5e5c955f16ee4256cb6a3e92d2e58029cd13d63
20HecSan <hectorsanchiii@gmail.com>
2023-10-18 23:22:50 -0400

    Confused

---
+++ 02-Challenge/Develop/db/store.js
@@ -0,0 +1,48 @@
+const util = require("util");
+const fs = require("fs");
+
+const readFileAsync = util.promisify(fs.readFile);
+const receiveFileAsync = util.promisify(fs.writeFile);
+// const readFileAsync = util.promisify(fs.readFile);
+
+//  `db.json` file on the backend that will be used to store and retrieve notes using the fs module.
+class Store {
+    read() {
+        return readFileAsync("./db/db.json", "utf8");
+    }
+
+    receive(note, callback) {
+        console.log("NOTE", note);
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) console.log(err);
+
+            let getData = JSON.parse(data);
+            getData.push(note);
+
+            fs.writeFile("./db/db.json", JSON.stringify(getData), err => {
+                if (err) {
+                    console.log(err);
+                }
+                return callback(getData);
+            });
+        });
+    }
+
+    delete(index, callback) {
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) console.log(err);
+
+            let getData = JSON.parse(data);
+            getData.splice(index, 1);
+
+            fs.writeFile("./db/db.json", JSON.stringify(getData), err => {
+                if (err) {
+                    console.log(err);
+                }
+                return callback(getData);
+            });
+        });
+    }
+}
+
+module.exports = Store;
